b04ba27985fa5553072024be89707ed0b0f0f06d
Aaliyah McCollum <aaliyahlynnette@outlook.com>
2025-11-21 15:33:40 -0800

    Homepage Button overlay CSS & Func

--- js/getMovie.js
+++ js/getMovie.js
@@ -16,8 +16,18 @@ list.forEach(movie => {
     const div = document.createElement("div");
     div.className = "movie-card";
     div.innerHTML = `
-    <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}">
-    <p>${movie.title}</p>
+    <div class="movie-card-inner">
+        <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}">
+        <div class="movie-overlay">
+            <button class="overlay-btn view-movie" data-id="${movie.id}">
+                View
+            </button>
+            <button class="overlay-btn add-watchlist" data-id="${movie.id}" data-title="${movie.title}" data-poster="${movie.poster_path}">
+                Watchlist   
+            </button>
+        </div>
+        <p>${movie.title}</p>
+    </div>
     `;
     container.appendChild(div);
 });
@@ -36,5 +46,20 @@ displayMovies(trendingMovies, "trending-movies");
 
 loadMovies();
 
+// JS For movie card buttons overlay (Add to Watchlist & Nav to Movie page)
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-movie")) {
+        const id = e.target.dataset.id;
+        window.location.href = `movie.html?id=${id}`;
+    }
+    if (e.target.classList.contains("add-watchlist")){
+        const movie = {
+            id: e.target.dataset.id,
+            title: e.target.dataset.title,
+            poster_path: e.target.dataset.poster
+        };
 
+        addToWatchList(movie)
+    }
+})
 
