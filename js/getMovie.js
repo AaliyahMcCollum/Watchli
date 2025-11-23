479e359628d8a404bcf1b6540e9f4a9cea11a8df
Joaquinrdlt <007605813@coyote.csusb.edu>
2025-11-23 11:35:54 -0800

    css for main page

--- js/getMovie.js
+++ js/getMovie.js
@@ -4,15 +4,15 @@ const fullImage = "https://image.tmdb.org/t/p/w500";
 const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 
 async function fetchMovies(url) {
-const res = await fetch(url);
-const data = await res.json();
-return data.results.slice(0, 7);
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.results.slice(0, 7);
 }
 
 function displayMovies(list, elementId) {
-const container = document.getElementById(elementId);
-container.innerHTML = "";
-list.forEach(movie => {
+  const container = document.getElementById(elementId);
+  container.innerHTML = "";
+  list.forEach((movie) => {
     const div = document.createElement("div");
     div.className = "movie-card";
     div.innerHTML = `
@@ -20,21 +20,18 @@ list.forEach(movie => {
     <p>${movie.title}</p>
     `;
     container.appendChild(div);
-});
+  });
 }
 
 async function loadMovies() {
-const newMoviesURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
-const trendingMoviesURL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+  const newMoviesURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
+  const trendingMoviesURL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
 
-const newMovies = await fetchMovies(newMoviesURL);
-const trendingMovies = await fetchMovies(trendingMoviesURL);
+  const newMovies = await fetchMovies(newMoviesURL);
+  const trendingMovies = await fetchMovies(trendingMoviesURL);
 
-displayMovies(newMovies, "new-movies");
-displayMovies(trendingMovies, "trending-movies");
+  displayMovies(newMovies, "new-movies");
+  displayMovies(trendingMovies, "trending-movies");
 }
 
 loadMovies();
-
-
-
